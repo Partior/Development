8ab06d50b63f01e9186670401e38a11d13484776
Alex M Granata <granataa@vt.edu>
2015-10-02 11:32:49 -0400

    Finally got my butt in gear to fix the intial estimation.
    I actaully feel pretty good about this, it uses a resize senseing function
    for the contraint plot to adjust linewidth sizese to the size of the
    figure, additionally adjusting the location of the legend based on size fo
    the figure to fit in the graph.
    
    Two graphs are output by power_constr.m:
    	the first, by cnstr.m, is the contraint graph with 8 seleactable
    	constraints.
    	the second, by power_constr.m creates a minimum power requried for
    	various manuevers.
    Notes: Need to fix the power requried, as I think I am outputting Power
    used, i.e., I am putting the manuever power, not the power avalibe
    neccesary for the power required... if that makes sense.

---
+++ cnstrnts/power_constr.m
@@ -0,0 +1,31 @@
+%% Power to Constraints
+% Designed to take T/W data from Constraint Graphic, and transform that to
+% Power required at Various Altitudes
+close all; clear; clc
+cnstr
+
+%% Cruise Conditions
+% Equilvant Power to various altitudes 
+D=@(ws,v,p) Cd0*0.5*p*v.^2.*(Wto./ws)+K*ws*Wto./(0.5*p*v.^2);
+
+% Straight, Level Flight
+Preq_cruise=D(WSdom,V_c,p_c)*V_c/(p_c/p_sl);
+% Service Ceiling
+Preq_serv=D(WSdom,V_md(WSdom,p_sc),p_sc).*V_md(WSdom,p_sc)/(p_sc/p_sl);
+% Cruise Ceiling
+Preq_cc=D(WSdom,V_md(WSdom,p_c),p_sc).*V_md(WSdom,p_c)/(p_c/p_sl);
+% 2.5g Maneuer at Sea Level
+Preq_man=D(WSdom,V_c,p_sl)*V_c/(p_sl/p_sl);
+
+%% Graphic Output
+f2=figure(2); clf
+hold on
+plot(WSdom,Preq_cruise/550,'b-')
+plot(WSdom,Preq_serv/550,'g-')
+plot(WSdom,Preq_cc/550,'k-')
+plot(WSdom,Preq_man/550,'r-.')
+
+legend({'Cruise','Ceiling_{Service}','Ceiling_{Cruise}','2.5g @ SL'})
+xlabel('Wing Loading, lbs/ft^2')
+ylabel('Required Power, hp')
+title('Partior Q==1 Power Requirements')
