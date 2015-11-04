b8d78d3f13158ddd74648bc77e90c3ff1a23644a
AMG <granataa@comcast.net>
2015-11-04 15:20:14 -0500

    fixed merge shit

--- Preconcepts/Performace/parm.m
+++ Preconcepts/Performace/parm.m
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 %% Parameters
 % This script assings variables to workspace to define the parameters and
 % goals that the Q1 will meet, as decided upon by the team.
@@ -14,7 +13,7 @@ sfc=0.55;
 % Weight
 % We=9087.8; % lbs empty/dry weight of aircraft
 % Wf=2663.3; % lbs max fuel capactiy
-We=8717;
+We=8717; % WTo=16,965
 Wf=2503;
 
 % Dimensions
@@ -42,49 +41,4 @@ p_sl=2.3769e-3;
 T0=1.1+04;
 % lbf, static thrust
 VTO=1.2*110;
-=======
-%% Parameters
-% This script assings variables to workspace to define the parameters and
-% goals that the Q1 will meet, as decided upon by the team.
-
-% Mission
-V_c=250;
-% RFP is 250 mph, so we bumped to 265 mph cruise velocity
-p_c=1.1043e-3;
-% airdensity at cruise altitude of 24,000 ft
-sfc=0.55;
-% Specifc fuel consumption
-
-% Weight
-% We=9087.8; % lbs empty/dry weight of aircraft
-% Wf=2663.3; % lbs max fuel capactiy
-We=8717;
-Wf=2503;
-
-% Dimensions
-AR=10;
-S=425;  % ft^2, makes WS at max WTO approx 40;
-b=65.2;    % ft, span
-
-% Aerodynamic Performance
-e=0.75;
-LD=21;
-% Lift over drag at cruise conditions
-
-% Power
-P=1000; % hp, provided by all engines
-
-% Takeoff conditions
-CLg=1.7;
-% Lift coefficient for takeoff (slats+flaps)
-CDg=0.05;
-% Drag coefficient for takeoff (slats+flaps)
-mu=0.04;
-% Should be able to take off from "firm turf"
-p_sl=2.3769e-3;
-% airdensity at sea level altitude
-T0=1.1+04;
-% lbf, static thrust
-VTO=1.2*110;
->>>>>>> c95d0c656b92fc28985e433b06102151be39b9c5
 % Takeoff Velocity, at 1.2 * 105 ft/s (stall speed)
--- Preconcepts/Performace/sr.m
+++ Preconcepts/Performace/sr.m
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 function [srout]=sr(h,V)
 
 parm
@@ -10,17 +10,4 @@ nup=0.9; %efficiency
 Dr=Cd0*0.5*ph*V.^2.*(WTO/S)+K*WTO/S./(0.5*ph*V.^2);
 Pr=Dr*V;
 
-=======
-function [srout]=sr(h,V)
-
-parm
-ph=p_sl*exp(-3.8329e-05*h);
-WTO=We+Wf+225*(3+19);
-K=1/(e*pi*AR);
-Cd0=0.022; %guess
-nup=0.9; %efficiency
-Dr=Cd0*0.5*ph*V.^2.*(WTO/S)+K*WTO/S./(0.5*ph*V.^2);
-Pr=Dr*V;
-
->>>>>>> c95d0c656b92fc28985e433b06102151be39b9c5
 srout=nup*V/(0.5*Pr);
