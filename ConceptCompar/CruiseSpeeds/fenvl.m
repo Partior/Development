fdd1ef087606e4f1af4066f38fe7cdf92739b052
Opera <granataa@vt.edu>
2015-11-12 20:38:32 -0500

    Renamed Folder Cruise -> CruiseSpeeds for clarification

---
+++ ConceptCompar/CruiseSpeeds/fenvl.m
@@ -0,0 +1,19 @@
+function out=fenvl(P,h,PAY)
+
+load('../constants.mat')
+Cd0=0.02;
+
+pc=p(h);
+Pa=P*sqrt(pc/p(0));
+
+r=roots([Cd0*S^2*pc^2,0,0,-2*Pa*S*pc,4*K*W0(PAY)^2]);
+r=r(imag(r)==0);
+
+mx=max(r);
+mn=min(r);
+
+if mn>mx
+    out=NaN(1,2);
+else
+    out=[mx,mn];
+end
