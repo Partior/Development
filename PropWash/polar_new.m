0b20895d37015f4d7f9103f022a4dedc10be39d5
Opera <granataa@vt.edu>
2015-11-23 12:47:48 -0500

    Adjustment to AoA effect of propwash

--- PropWash/polar_new.m
+++ PropWash/polar_new.m
@@ -28,6 +28,7 @@ cd_new      % sets up airfoil drag polar
 b=sqrt(AR*S);
 chrd=S/b;
 beta=@(m) sqrt(1-m.^2);
+
 % How much of free stream wind is the AoA versus zero degress from prop?
 % impercial formula made up
 ang=@(v,h) v/v2(v,T(v,h),h);
@@ -40,7 +41,7 @@ L=@(aoa,h,v,on) (...
 
 % For induced drag, we are taking that the lift of the fuselage is 10% of
 % entire lift force, and that Wing lift is 80% of lift Force
-Df=@(aoa,h,v) 1/2*p(h)*v^2*(CD0_plane(v,h)+(K*Cla(aoa)^2)*0.1)/beta(v/a(h))*(80*5); % 80*5 is length * diamter for approx surface area
+Df=@(aoa,h,v) 1/2*p(h)*v^2*(CD0_plane(v,h)+(K*Cla(aoa)^2)*0.1)/beta(v/a(h))*(70*5); % 80*5 is length * diamter for approx surface area
 Dw=@(aoa,h,v,on) (...
     1/2*p(h)*v^2*Cda(aoa)*((b-2*Rmax*on)*chrd)+...free stream wing drag
     1/2*p(h)*v2(v,T(v,h),h)^2*Cda(aoa*ang(v,h))*((2*Rmax*on)*chrd)); % prop wash drag
@@ -54,7 +55,7 @@ gmma=@(aoa,h,v,on) v./(SFC/3600*(D(aoa,h,v,on)));
 % Plot various number of engines at 100 and 75% power levels
 % Stall line for each
 
-resol=40;
+resol=20;
 [m_msh,h_msh]=meshgrid(linspace(0.1,0.5,resol),linspace(0,40e3,resol));
 taoa=zeros(resol);
 pl=zeros(resol);
