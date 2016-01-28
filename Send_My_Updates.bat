be141f46cfb2107682db13e84c6077f54f5abc24
AMG <granataa@comcast.net>
2016-01-27 21:45:29 -0500

    Updated Send_My_Updates.bat to ask for a message for commit description

--- Send_My_Updates.bat
+++ Send_My_Updates.bat
@@ -1,6 +1,5 @@
 git add -A
 cls
-set /p mm= Please provide a brief description of your changes:  
-
+set /p mm= Please provide a brief description of your changes:
 git commit -m "%mm%"
-git push -q online
+git push online
